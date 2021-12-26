ba424a62e5a8fa5964dfc0c8778c24fb1ffda9c5
WhatTheDuck <41006973+xuandat282@users.noreply.github.com>
2021-12-26 14:44:01 +0700

    update

---
+++ main.js
@@ -0,0 +1,11 @@
+var add_data_points_manually = document.getElementById("add-data-points-manually")
+var add_data_points_randomly = document.getElementById("add-data-points-randomly")
+var add_data_points_randomly_count = document.getElementById("add-data-points-randomly-count")
+var add_data_points_from_file = document.getElementById("add-data-points-from-file")
+var remove_all_data_points = document.getElementById("remove-all-data-points")
+
+var add_k_manually = document.getElementById("add-k-manually")
+var add_k_randomly = document.getElementById("add-k-randomly")
+var add_k_randomly_count = document.getElementById("add-k-randomly-count")
+var remove_all_k = document.getElementById("remove-all-k")
+
