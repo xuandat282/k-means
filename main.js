df39f98a53b2f04314b8a772d90c50b20d0dabdf
WhatTheDuck <41006973+xuandat282@users.noreply.github.com>
2021-12-26 23:42:37 +0700

    update 26/12

--- main.js
+++ main.js
@@ -17,6 +17,10 @@ let data_points = [],
 
 add_data_points_randomly.addEventListener("click", () => randomDataPoints(add_data_points_randomly_count.value), false);
 remove_all_data_points.addEventListener("click", removeAllDataPoints, false);
+
+add_centroids_randomly.addEventListener("click", () => randomCetroids(add_centroids_randomly_count.value), false);
+remove_all_centroids.addEventListener("click", removeAllCentroids, false);
+
 // The number of data points to be added
 function randomDataPoints(count) {
     for (let i = 0; i < count; i++) {
@@ -34,6 +38,7 @@ function drawAll() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     // draw data points
     data_points.map(drawDataPoints);
+    centroids.map(drawCentroids);
 }
 
 function drawDataPoints([x, y]) {
@@ -45,5 +50,28 @@ function drawDataPoints([x, y]) {
 //remove all data points
 function removeAllDataPoints() {
     data_points = [];
+    drawAll();
+}
+// add random centroid
+function randomCetroids(count) {
+    for (let i = 0; i < count; i++) {
+        let newPoint = [
+            Math.floor(Math.random() * canvas.width),
+            Math.floor(Math.random() * canvas.height)
+        ];
+        centroids.push(newPoint);
+    }
+    drawAll();
+}
+
+function drawCentroids([x, y]) {
+    ctx.beginPath();
+    ctx.arc(x, y, 6, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fill();
+}
+
+function removeAllCentroids() {
+    centroids = [];
     drawAll();
 }
