c24900587f10cdd95c747cf48f0577555084d3f0
WhatTheDuck <41006973+xuandat282@users.noreply.github.com>
2021-12-26 23:29:13 +0700

    update 26/12

--- main.js
+++ main.js
@@ -9,40 +9,41 @@ var add_centroids_randomly = document.getElementById("add-centroids-randomly")
 var add_centroids_randomly_count = document.getElementById("add-centroids-randomly-count")
 var remove_all_centroids = document.getElementById("remove-all-centroids")
 
+
 var canvas = document.getElementById("canvas");
 ctx = canvas.getContext("2d");
 let data_points = [],
     centroids = [];
 
+add_data_points_randomly.addEventListener("click", () => randomDataPoints(add_data_points_randomly_count.value), false);
+remove_all_data_points.addEventListener("click", removeAllDataPoints, false);
 // The number of data points to be added
 function randomDataPoints(count) {
-    // random data points
     for (let i = 0; i < count; i++) {
         let newPoint = [
-            Math.floor(Math.random() * canvas.width - 1),
-            Math.floor(Math.random() * canvas.height - 1)
+            Math.floor(Math.random() * canvas.width),
+            Math.floor(Math.random() * canvas.height)
         ];
         data_points.push(newPoint);
     }
-}
-
-
-
-add_data_points_randomly.addEventListener("click", drawRandomDataPoints(add_data_points_randomly_count.value));
-
-// draw random data points
-function drawRandomDataPoints(count) {
-    randomDataPoints(count);
     drawAll();
 }
 
 function drawAll() {
-    data_points.map(drawDataPoints)
+    // clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // draw data points
+    data_points.map(drawDataPoints);
 }
 
 function drawDataPoints([x, y]) {
     ctx.beginPath();
-    ctx.arc(x, y, 5, 0, 2 * Math.PI);
+    ctx.arc(x, y, 3, 0, 2 * Math.PI);
     ctx.fill();
 }
-    
+
+//remove all data points
+function removeAllDataPoints() {
+    data_points = [];
+    drawAll();
+}
